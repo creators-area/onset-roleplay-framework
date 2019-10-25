60f51ef05ec10d0561ba1988c236905d658d91e5
Bastien Deshayes <bastien35@gmail.com>
2019-10-25 10:40:37 +0200

    :art: Add config file handler to utils package

--- packages/orf_utils/server/file.lua
+++ packages/orf_utils/server/file.lua
@@ -7,3 +7,22 @@ function file_exist( path )
 	return false
 end
 AddFunctionExport( 'file_exist', file_exist )
+
+function get_config( package_name, category )
+	local full_file_path = string.format( 'packages/%s/config.json', package_name )
+	if ( not file_exist( full_file_path ) ) then
+		ServerExit()
+		error( ( 'Cannot find the following config file: {%s}' ):format( full_file_path ) )
+	end
+
+	local stream = io.open( full_file_path )
+	local content = json_decode( stream:read( '*a' ) )
+	stream:close()
+
+	if ( category ~= nil and content[ category ] ~= nil ) then
+		content = content[ category ]
+	end
+
+	return content or {}
+end
+AddFunctionExport( 'get_config', get_config )
