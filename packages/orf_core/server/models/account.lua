122cf2e7dd1b487ac0df09c09f1452277ee2232a
Bastien Deshayes <bastien35@gmail.com>
2019-10-26 09:40:32 +0200

    :sparkles: Add a BaseModel and Account model

---
+++ packages/orf_core/server/models/account.lua
@@ -0,0 +1,74 @@
+local utils = ImportPackage( 'orf_utils' )
+local BaseModel = require( 'packages/' .. GetPackageName() .. '/server/models/base_model' )
+local Account = setmetatable( { }, { __index = BaseModel } )
+Account.__index = Account
+
+BaseModel._fields = {
+	[ 'SteamId' ] 		= { type = 'string', field = 'steamid', is_primary_key = true, updatable = false },
+	[ 'SteamName' ] 	= { type = 'string', field = 'steam_name' },
+	[ 'GameVersion' ] 	= { type = 'int', field = 'game_version' },
+	[ 'Locale' ] 		= { type = 'string', field = 'locale' },
+	[ 'CountLogin' ] 	= { type = 'int', field = 'count_login' },
+	[ 'CountKick' ] 	= { type = 'int', field = 'count_kick' },
+	[ 'LastIp' ] 		= { type = 'string', field = 'last_ip' },
+	[ 'CreatedAt' ] 	= { type = 'int', field = 'created_at', updatable = false }
+}
+
+BaseModel._table = 'accounts'
+
+function Account.new( primary_key )
+	return setmetatable( BaseModel.new( primary_key ), Account )
+end
+
+function Account:IncrementLoginCount()
+	self:SetCountLogin( math.tointeger( self:GetCountLogin() ) + 1 )
+	return self
+end
+
+function Account:LoadRoles()
+
+end
+
+function Account:AddRole( role_name )
+
+end
+
+function Account:RemoveRole( role_name )
+
+end
+
+function Account:Load( callback, ... )
+	local vargs = { ... }
+	local this = self
+	QueryBuilder:new():select( '*' ):from( self._table ):where( self._PrimaryKeyField, '=', self:GetPrimaryKey() ):exec(function( results, extras )
+		results = results[ 1 ]
+		for k, v in pairs( self._fields ) do
+			if ( results[ v.field ] ~= nil ) then
+				self[ 'Set' .. k ]( self, results[ v.field ] )
+			end
+		end
+		callback( results, extras, table.unpack( vargs ) )
+	end)
+end
+
+function Account:Update( player, callback )
+	self:SetGameVersion( GetPlayerGameVersion( player ) )
+	self:SetSteamName( GetPlayerName( player ) )
+	self:SetLocale( GetPlayerLocale( player ) )
+	self:SetLastIp( GetPlayerIP( player ) )
+	self:DbUpdate( callback, player )
+end
+
+function Account:Register( player, callback )
+	self:SetSteamId( tostring( GetPlayerSteamId( player ) ) )
+	self:SetGameVersion( GetPlayerGameVersion( player ) )
+	self:SetSteamName( GetPlayerName( player ) )
+	self:SetLocale( GetPlayerLocale( player ) )
+	self:SetCountLogin( 1 )
+	self:SetCountKick( 0 )
+	self:SetLastIp( GetPlayerIP( player ) )
+	self:SetCreatedAt( utils.get_unix_time() )
+	self:DbSave( callback, player )
+end
+
+return Account
---
+++ packages/orf_core/server/models/base_model.lua
@@ -0,0 +1,65 @@
+local database = ImportPackage( 'orf_database' )
+local utils = ImportPackage( 'orf_utils' )
+
+local BaseModel = {}
+BaseModel.__index = BaseModel
+
+local function init_accessors( model )
+	for k, v in pairs( model._fields ) do
+		if ( v.is_primary_key ) then model._PrimaryKeyField = v.field end
+		model[ 'Get' .. k ] = function() return model[ '_' .. v.field ] or nil end
+		model[ 'Set' .. k ] = function( s, value )
+			model[ '_' .. v.field ] = value
+		end
+	end
+end
+
+local function get_updatable_field( model )
+	local fields = {}
+	for k, v in pairs( model._fields ) do
+		if ( not v.is_primary_key and v.updatable == nil or v.updatable ) then
+			fields[ v.field ] = model[ 'Get' .. k ]()
+		end
+	end
+	return fields
+end
+
+local function get_insertable_field( model )
+	local fields = {}
+	for k, v in pairs( model._fields ) do
+		if ( v.insertable == nil or v.insertable ~= nil and v.insertable ) then
+			fields[ v.field ] = model[ 'Get' .. k ]()
+		end
+	end
+	return fields
+end
+
+function BaseModel.new( primary_key )
+	local self = setmetatable( {
+		_primaryKey = primary_key
+	}, BaseModel )
+	init_accessors( self )
+	return self
+end
+
+function BaseModel:DbUpdate( callback, ... )
+	local vargs = { ... }
+	local updatable_fields = get_updatable_field( self )
+	QueryBuilder:new():update( self._table, updatable_fields ):where( self._PrimaryKeyField, '=', self:GetPrimaryKey()):exec(function( results, extras )
+		if ( type( callback ) == 'function' ) then callback( results, extras, table.unpack( vargs ) ) end
+	end) 
+end
+
+function BaseModel:DbSave( callback, ... )
+	local vargs = { ... }
+	local insertable_fields = get_insertable_field( self )
+	QueryBuilder:new():insert( self._table, insertable_fields ):exec(function( results, extras )
+		if ( type( callback ) == 'function' ) then callback( results, extras, table.unpack( vargs ) ) end
+	end)
+end
+
+function BaseModel:GetPrimaryKey()
+	return self._primaryKey
+end
+
+return BaseModel
