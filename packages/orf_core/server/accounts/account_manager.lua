9093acd35e7a394edc5db4adcec1482d6273ab49
Bastien Deshayes <bastien35@gmail.com>
2019-10-26 09:40:58 +0200

    :truck: Move authentication file

---
+++ packages/orf_core/server/accounts/account_manager.lua
@@ -0,0 +1,28 @@
+AccountManager = {}
+
+local accounts = {}
+
+function AccountManager:Add( player_id, account )
+	accounts[ player_id ] = account
+	return accounts[ player_id ]
+end
+
+function AccountManager:Get( player_id )
+	return accounts[ player_id ] or {}
+end
+
+function AccountManager:GetAll()
+	return accounts or {}
+end
+
+function AccountManager:Remove( player_id )
+	accounts[ player_id ] = {}
+end
+
+function AccountManager:HaveRole( player_id, role_name )
+
+end
+
+function AccountManager:HavePermissionTo( player_id, permission_name )
+
+end
