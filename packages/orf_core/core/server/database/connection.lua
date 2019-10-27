9f8c794008384e238fbef64651a1d50821e982ec
Bastien Deshayes <bastien35@gmail.com>
2019-10-27 19:59:27 +0100

    :sparkles: Insert default permissions and roles on server start

--- packages/orf_core/core/server/database/connection.lua
+++ packages/orf_core/core/server/database/connection.lua
@@ -7,6 +7,20 @@ function get_connection()
 end
 AddFunctionExport( 'get_connection', get_connection )
 
+local function insert_roles_and_permissions()
+	local permissions = utils.get_config( GetPackageName(), 'permissions' )
+	for i = 1, #permissions do
+		local permission = permissions[ i ]
+		QueryBuilder:new():raw( 'INSERT IGNORE INTO `permissions` ( `name`, `description` ) VALUES ( \'?\', \'?\' )', permission.name, permission.description ):exec()
+	end
+
+	local roles = utils.get_config( GetPackageName(), 'roles' ).roles
+	for i = 1, #roles do
+		local role = roles[ i ]
+		QueryBuilder:new():raw( 'INSERT IGNORE INTO `roles` ( `name`, `description` ) VALUES ( \'?\', \'?\' )', role.name, role.description ):exec()
+	end
+end
+
 local function OnPackageStart()
 	local credentials = utils.get_config( GetPackageName(), 'database' )
 	mariadb_log( credentials.log_level )
@@ -18,6 +32,7 @@ local function OnPackageStart()
 		if ( utils.file_exist( credentials.script_path ) ) then
 			mariadb_await_query_file( connection, credentials.script_path )
 		end
+		insert_roles_and_permissions()
 	else
 		ServerExit()
 		error( 'Cannot connect to server, please check your credentials in config.json' )
