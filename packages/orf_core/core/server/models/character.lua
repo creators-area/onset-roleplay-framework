56f2d9ecf85856145e4b8674bfb9b5b7a732fbb6
Bastien Deshayes <bastien35@gmail.com>
2019-10-29 15:25:20 +0100

    :bug: Now get characters with new database system

--- packages/orf_core/core/server/models/character.lua
+++ packages/orf_core/core/server/models/character.lua
@@ -13,16 +13,17 @@ function Character.new( primary_key )
 end
 
 function Character:Load( callback, ... )
-	-- local vargs = { ... }
-	-- QueryBuilder:new():select( '*' ):from( self._table ):where( self._PrimaryKeyField, '=', self:GetPrimaryKey() ):exec(function( results, extras )
-	-- 	results = results[ 1 ]
-	-- 	for k, v in pairs( self._fields ) do
-	-- 		if ( results[ v.field ] ~= nil ) then
-	-- 			self[ 'Set' .. k ]( self, results[ v.field ] )
-	-- 		end
-	-- 	end
-	-- 	callback( results, extras, table.unpack( vargs ) )
-	-- end)
+	local vargs = { ... }
+	local query ( 'SELECT * FROM `%s` WHERE %s = ? LIMIT 1' ):format( self._table, self._PrimaryKeyField )
+	database.asyncQuery( query, { self:GetPrimaryKey() }, function( results )
+		results = results[ 1 ]
+		for k, v in pairs( self._fields ) do
+			if ( results[ v.field ] ~= nil ) then
+				self[ 'Set' .. k ]( self, results[ v.field ] )
+			end
+		end
+		callback( results, table.unpack( vargs ) )
+	end)
 end
 
 function Character:Update( player, callback )
